14cef62f1d7876694d4db7ddd02ae7f923b846bb
YuxinSunny <463290393@qq.com>
2017-06-05 23:19:05 +0800

    失败场景

---
+++ WetherToStartAgain.h
@@ -0,0 +1,27 @@
+#pragma once
+#pragma once
+#ifndef __WETHERTOSTARTAGAIN_SCENE_H__
+#define __WETHERTOSTARTAGAIN_SCENE_H__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class WetherToStartAgain : public cocos2d::Layer
+{
+
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	// a selector callback
+	void menuCloseCallback(cocos2d::Ref* pSender);
+
+	void menuContinueCallback(cocos2d::Ref* pSender);
+
+
+	// implement the "static create()" method manually
+	CREATE_FUNC(WetherToStartAgain);
+};
+
+#endif // __WETHERTOSTARTAGAIN_SCENE_H__
