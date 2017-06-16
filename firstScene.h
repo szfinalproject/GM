3d8f4ae5d445a88db78508d3c7607d78cf54b2fb
YuxinSunny <463290393@qq.com>
2017-06-16 17:09:06 +0800

    没用了
    没用了

--- firstScene.h
+++ firstScene.h
@@ -1,28 +1,2 @@
 
 
-#ifndef __FIRSTSCENE_SCENE_H__
-#define __FIRSTSCENE_SCENE_H__
-
-#include "cocos2d.h"
-USING_NS_CC;
-
-class firstScene : public cocos2d::Layer
-{
-	
-public:
-	static cocos2d::Scene* createScene();
-
-	virtual bool init();
-
-	// a selector callback
-	void menuCloseCallback(cocos2d::Ref* pSender);
-
-	
-
-
-	// implement the "static create()" method manually
-	CREATE_FUNC(firstScene);
-};
-
-#endif // __FIRSTSCENE_SCENE_H__
-
